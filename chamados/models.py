beca4d48ebbf639bed74f10b3adbe0946e6ae175
pontesvjp <pontes.vjp@gmail.com>
2024-10-27 16:17:04 -0300

    incluindo banco de dados sqlite

--- chamados/models.py
+++ chamados/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Chamado(models.Model):
+    titulo = models.CharField(max_length=100, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    status = models.CharField(max_length=100, null=False, blank=False)
+    
+    def __str__(self):
+        return f"Chamado [titulo={self.titulo}]"
--- chamados/views.py
+++ chamados/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from chamados.models import Chamado
 
 
 def index(request):
-    return render(request, 'chamados/index.html')
+    chamados = Chamado.objects.all()
+
+    return render(request, 'chamados/index.html', {"cards": chamados})
+
 
 def novo_chamado(request):
     return render(request, 'chamados/novo_chamado.html')
